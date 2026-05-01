e47ea4495cb1bf1ec6466148b82c5d8fbf85e521
Deniz Günes <denizlg24@gmail.com>
2026-05-01 20:40:51 +0100

    chore: install nextjs, better auth, drizzle with pg, sets up envoy

---
+++ db/connection.ts
@@ -0,0 +1,11 @@
+
+
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+export const db = drizzle({ 
+  connection: { 
+    connectionString: process.env.DATABASE_URL!,
+    ssl: true
+  }
+});
